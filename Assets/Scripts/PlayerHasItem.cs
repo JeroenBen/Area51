4ffd5397572fdb14e4990cb8b443232597a6bc32
JeroenBen <59441635+JeroenBen@users.noreply.github.com>
2020-03-13 23:42:33 +0100

    Going back to save files

--- Assets/Scripts/PlayerHasItem.cs
+++
@@ -1,9 +0,0 @@
-﻿[System.Serializable]
-public class PlayerHasItem : WinCondition
-{
-    public int itemId;
-    public override bool PlayerWins() {
-        Inventory inventory = LevelManager.Instance.Player.GetComponent<Inventory>();
-        return inventory.characterItems.Exists(x => x.id == itemId);
-    }
-}
--- Assets/Scripts/WinCondition.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[System.Serializable]
-public abstract class WinCondition : MonoBehaviour
-{
-    public abstract bool PlayerWins();
-}
