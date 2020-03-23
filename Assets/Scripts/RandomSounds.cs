7924937732fd6a62c94a941c851c6e0d1da40e05
JeroenBen <59441635+JeroenBen@users.noreply.github.com>
2020-03-24 00:48:33 +0100

    UpdateSmall

---
+++ Assets/Scripts/RandomSounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSounds : MonoBehaviour
+{
+	public List<AudioClip> sounds;
+	public AudioSource source;
+	public int randomSpread, wait;
+	// Use this for initialization
+	int timer;
+	void Start() {
+		timer = (int)(Random.value * randomSpread);
+
+	}
+	// Update is called once per frame
+	void FixedUpdate() {
+		if (timer <= 0) {
+			source.PlayOneShot(sounds[Random.Range(0, sounds.Count)]);
+
+			timer = wait + (int)(Random.value * randomSpread);
+			print(timer);
+		}
+
+
+
+		timer--;
+	}
+}
